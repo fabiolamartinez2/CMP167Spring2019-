9b82c25d3d7ed85acd948320fd0c4c422edaf1e8
fabiolamartinez2 <47428213+fabiolamartinez2@users.noreply.github.com>
2019-03-14 12:32:41 -0400

    Add files via upload

--- Area.java
+++ Area.java
@@ -1,9 +1,42 @@
+/*
+ * Fabiola Martinez
+ * This program implements area function/method
+ */
 
+
+
+import java.util.Scanner;
 public class Area {
+	static Scanner keyboard = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static int getRecArea(int width, int length){
+	return width * length;
+	
+	} //Circle area
+	
+	public static double getCircleArea(double radius, double area ) {
+		return radius * radius * 3.13;
+		
+	} // traingle area
+	
+	public static int getTriangleArea( int base, int height) {
+		
+		   return ((base* height)/2);
 	}
-
+	
+	public static void main(String[] args) {
+		rectArea();
+		circArea();
+		trainArea();
+		System.out.println("Enter width and length: ");
+	   int width = keyboard.nextInt();
+	    int length = keyboard.nextInt();
+	    int area = getRecArea (width, length);
+	    
+	    System.out.printlm("Area: " + area);
+	    
+	    
+	    
+	    
+	}
 }
